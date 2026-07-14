85a934977313934c7deeb33c3102fff1d517863e
ethan-hann <36464732+ethan-hann@users.noreply.github.com>
2026-07-14 14:46:56 -0500

    implement shooting and fix update bug

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
 from circleshape import CircleShape
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x: float, y: float):
@@ -24,6 +25,12 @@ class Player(CircleShape):
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
     
+    def shoot(self) -> None:
+        shot = Shot(self.position.x, self.position.y)
+        unit_vect = pygame.Vector2(0, 1)
+        unit_vect_rotated = unit_vect.rotate(self.rotation)
+        shot.velocity = unit_vect_rotated * PLAYER_SHOOT_SPEED
+    
     def update(self, dt: float) -> None:
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
@@ -34,6 +41,8 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
         
     def draw(self, screen: pygame.Surface) -> None:
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
--- shot.py
+++ shot.py
@@ -1,10 +1,10 @@
 import pygame
+from constants import SHOT_RADIUS, LINE_WIDTH
 from circleshape import CircleShape
-from constants import LINE_WIDTH
 
-class Asteroid(CircleShape):
-    def __init__(self, x: float, y: float, radius: float) -> None:
-        super().__init__(x, y, radius)
+class Shot(CircleShape):
+    def __init__(self, x: float, y: float) -> None:
+        super().__init__(x, y, SHOT_RADIUS)
         
     def draw(self, screen: pygame.Surface) -> None:
         pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
